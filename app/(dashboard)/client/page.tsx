8e3f509a250324388ea9ab988fc1eb2bff84b644
ambrose-kibet <ambrosekibet36@gmail.com>
2024-01-16 21:19:05 +0300

    Add client component and session handling

--- app/(dashboard)/client/page.tsx
+++ app/(dashboard)/client/page.tsx
@@ -1,4 +1,13 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import UserInfo from '@/components/userInfo';
+import { useSession, signOut } from 'next-auth/react';
+
 const ClientPage = () => {
-  return <div>ClientPage</div>;
+  // Accessing session in client components
+  const session = useSession();
+
+  return <UserInfo user={session.data?.user} label="📱 Client component" />;
 };
 export default ClientPage;
