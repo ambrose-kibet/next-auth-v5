7cd5a1d7bb3af764f85df0f33e02422e6b2e48bc
ambrose-kibet <ambrosekibet36@gmail.com>
2024-01-17 21:20:14 +0300

    Add account information to session and update user properties

--- auth.ts
+++ auth.ts
@@ -5,12 +5,14 @@ import prisma from '@/lib/db';
 import { getUserById } from '@/data/user';
 import { UserRole } from '@prisma/client';
 import { getTwoFactorAuthenticationByUserId } from '@/data/two-factor-authentication';
+import { getAccountByUserId } from './data/account';
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
+  update,
 } = NextAuth({
   pages: {
     error: '/auth/error',
@@ -62,9 +64,13 @@ export const {
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
-      if (token.isTwoFactorEnabled && session.user) {
-        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled;
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.isOAuth = token.isOAuth as boolean;
       }
+
       return session;
     },
     async jwt({ token, user }) {
@@ -75,8 +81,13 @@ export const {
       if (!existingUser) {
         return token;
       }
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
-      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
+      token.isTwoFactorEnabled = !!existingUser.isTwoFactorEnabled;
       return token;
     },
   },
